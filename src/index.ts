edbb93fd3fd4cb5ac5c19f3378f9993e0e76427f
Shri Hari M <shrihari.2041@gmail.com>
2023-08-17 11:25:18 +0530

    final v6

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { AzureDevOpsExtension, IExtensionContext } from '@microsoft/azure-devops-extension-sdk';
-
-export class MyExtension extends AzureDevOpsExtension {
-  onInit(context: IExtensionContext) {
-    console.log('The extension has been initialized!');
-  }
-}
